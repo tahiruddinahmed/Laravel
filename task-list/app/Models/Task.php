79097c4948705e1d41f344a52cfc7e2705b2589a
TahirUddinAhmed <01tahirahmed@gmail.com>
2024-11-04 21:03:48 +0530

    Fillable

--- task-list/app/Models/Task.php
+++ task-list/app/Models/Task.php
@@ -10,6 +10,6 @@ class Task extends Model
     use HasFactory;
 
     protected $fillable = ['title', 'description', 'long_description'];
-    // protected $guarded = ['password']
+    // protected $guarded = ['secret']
 
 }
